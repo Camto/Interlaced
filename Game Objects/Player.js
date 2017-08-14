540ce0cd704e2f7bb3e1e288c434a5da0015688c
Benjamin Philippe Applegate <www.ben.pr@gmail.com>
2017-08-13 20:29:41 -0400

    Jumping is cool!

--- Game Objects/Player.js
+++ Game Objects/Player.js
@@ -5,6 +5,8 @@ function Player(x, y) { // The player class, the only manually controllable game
 	this.xs = 0;
 	this.ys = 0;
 	
+	this.on_floor = false;
+	
 }
 
 Player.prototype = Object.create(Game_Object.prototype); // Also inheritance.
@@ -13,43 +15,60 @@ Player.prototype.constructor = "Player";
 Player.prototype.update = function(world) {
 	
 	if(world) {
-		
 		if(keys[key_codes.a]) {
-			
 			this.xs -= 0.5;
-			
 		}
-		
 		if(keys[key_codes.d]) {
-			
 			this.xs += 0.5;
-			
 		}
-		
 	} else {
-		
 		if(keys[key_codes.left]) {
-			
 			this.xs -= 0.5;
-			
 		}
-		
 		if(keys[key_codes.right]) {
-			
 			this.xs += 0.5;
-			
 		}
-		
 	}
 	
 	this.xs *= 0.8;
+	
+	if(this.on_floor) {
+		if(world) {
+			if(keys[key_codes.w]) {
+				this.ys = -4;
+			}
+		} else {
+			if(keys[key_codes.up]) {
+				this.ys = -4;
+			}
+		}
+	}
+	
+	this.ys += 0.25;
+	
 	this.x += this.xs;
 	
 	if(world) {
 		
 		for(var count = 0; count < light_objects.blocks.length; count++) {
 			if(this.collide(light_objects.blocks[count])) {
-				this.x = light_objects.blocks[count].x - this.w;
+				if(this.xs > 0) {
+					this.x = light_objects.blocks[count].x - this.w;
+				} else {
+					this.x = light_objects.blocks[count].x + light_objects.blocks[count].w;
+				}
+				this.xs = 0;
+			}
+		}
+		
+		for(var count = 0; count < light_objects.walls.length; count++) {
+			if(this.collide(light_objects.walls[count])) {
+				if(this.xs > 0) {
+					this.x = light_objects.walls[count].x - this.w;
+				} else {
+					this.x = light_objects.walls[count].x + light_objects.walls[count].w;
+				}
+				this.xs = 0;
 			}
 		}
 		
@@ -57,7 +76,80 @@ Player.prototype.update = function(world) {
 		
 		for(var count = 0; count < dark_objects.blocks.length; count++) {
 			if(this.collide(dark_objects.blocks[count])) {
-				this.x = dark_objects.blocks[count].x - this.w;
+				if(this.xs > 0) {
+					this.x = dark_objects.blocks[count].x - this.w;
+				} else {
+					this.x = dark_objects.blocks[count].x + dark_objects.blocks[count].w;
+				}
+				this.xs = 0;
+			}
+		}
+		
+		for(var count = 0; count < dark_objects.walls.length; count++) {
+			if(this.collide(dark_objects.walls[count])) {
+				if(this.xs > 0) {
+					this.x = dark_objects.walls[count].x - this.w;
+				} else {
+					this.x = dark_objects.walls[count].x + dark_objects.walls[count].w;
+				}
+				this.xs = 0;
+			}
+		}
+		
+	}
+	
+	this.y += this.ys;
+	this.on_floor = false;
+	
+	if(world) {
+		
+		for(var count = 0; count < light_objects.blocks.length; count++) {
+			if(this.collide(light_objects.blocks[count])) {
+				if(this.ys > 0) {
+					this.y = light_objects.blocks[count].y - this.h;
+					this.on_floor = true;
+				} else {
+					this.y = light_objects.blocks[count].y + light_objects.blocks[count].h;
+				}
+				this.ys = 0;
+			}
+		}
+		
+		for(var count = 0; count < light_objects.walls.length; count++) {
+			if(this.collide(light_objects.walls[count])) {
+				if(this.ys > 0) {
+					this.y = light_objects.walls[count].y - this.h;
+					this.on_floor = true;
+				} else {
+					this.y = light_objects.walls[count].y + light_objects.walls[count].h;
+				}
+				this.ys = 0;
+			}
+		}
+		
+	} else {
+		
+		for(var count = 0; count < dark_objects.blocks.length; count++) {
+			if(this.collide(dark_objects.blocks[count])) {
+				if(this.ys > 0) {
+					this.y = dark_objects.blocks[count].y - this.h;
+					this.on_floor = true;
+				} else {
+					this.y = dark_objects.blocks[count].y + dark_objects.blocks[count].h;
+				}
+				this.ys = 0;
+			}
+		}
+		
+		for(var count = 0; count < dark_objects.walls.length; count++) {
+			if(this.collide(dark_objects.walls[count])) {
+				if(this.ys > 0) {
+					this.y = dark_objects.walls[count].y - this.h;
+					this.on_floor = true;
+				} else {
+					this.y = dark_objects.walls[count].y + dark_objects.walls[count].h;
+				}
+				this.ys = 0;
 			}
 		}
 		
@@ -65,7 +157,7 @@ Player.prototype.update = function(world) {
 	
 };
 
-Player.prototype.draw = function(pencil) {
+Player.prototype.draw = function(pencil) { // Render the player.
 	
 	pencil.beginPath();
 	pencil.fillStyle = (pencil.world ? "#0076D6" : "#FF8929");
